c991b9b9741d809d31f43862c08e92429030cac7
renzon <renzon@gmail.com>
2018-05-16 16:23:36 -0300

    Criado teste de exceção

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,3 +1,9 @@
 class Enviador:
     def enviar(self, remetente, destinatario, assunto, corpo):
+        if '@' not in remetente:
+            raise EmailInvalido(f'Email de remetente inválido: {remetente}')
         return remetente
+
+
+class EmailInvalido(Exception):
+    pass
--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,6 +1,6 @@
 import pytest
 
-from libpythonpro.spam.enviador_de_email import Enviador
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
@@ -21,3 +21,18 @@ def test_remetente(remetente):
         'Primeira turma Guido Von Rossum aberta.'
     )
     assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'renzo']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'luciano@python.pro.br',
+            'Cursos Python Pro',
+            'Primeira turma Guido Von Rossum aberta.'
+        )

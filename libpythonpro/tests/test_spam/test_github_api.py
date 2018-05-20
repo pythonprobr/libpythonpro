ae87981fcdbb062866aa836fd1483e7cc86d4e2c
renzon <renzon@gmail.com>
2018-05-20 20:25:59 -0300

    Isolado Import manualmente

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,29 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars3.githubusercontent.com/u/402714?v=4'
     resp_mock.json.return_value = {
         'login': 'renzo', 'id': 402714,
-        'avatar_url': 'https://avatars3.githubusercontent.com/u/402714?v=4',
+        'avatar_url': url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('renzo')
-    assert 'https://avatars3.githubusercontent.com/u/402714?v=4' == url
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renzon')
+    assert 'https://avatars3.githubusercontent.com/u/3457115?v=4' == url

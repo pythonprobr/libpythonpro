68ba8d1aa9ddaa6b07c03d5f37d1db5d51c74a6d
Eder Vasco Pinheiro <eder.pinheiro@wplex.com.br>
2019-07-22 12:11:13 -0300

    usando pacote requests e criado arquivo de depenências

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -11,3 +11,7 @@ def buscar_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('evpinheiro'))
+

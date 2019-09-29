aff2813c5e1a58239d871da92eee07bea384c953
undersfx <atchin_@hotmail.com>
2019-09-28 23:27:12 -0300

    Workaround para problema no parser da licença

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license=read('LICENSE'),
+    license="GNU AFFERO GENERAL PUBLIC LICENSE",
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),

2884c25b49eaf8c19b52c2c63db572cc6175026d
Lnvictor <Lnvictor@github.com>
2019-11-10 16:31:39 -0300

    pyup

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -2,12 +2,9 @@ import requests
 
 
 def buscar_avatar(usuario):
-    """
-    Busca o avatar de um usuário no Github
-
-    :param usuario: str com o nome de usuário no github
-    :return: str com o link do avatar
-    """
     url = f'https://api.github.com/users/{usuario}'
-    resp = requests.get(url)
-    return resp.json()['avatar_url']
+    return requests.get(url).json()['avatar_url']
+
+
+if __name__ == "__main__":
+    print(buscar_avatar('SdReiss'))

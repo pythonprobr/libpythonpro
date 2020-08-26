b4ecaa27cc006ac65afe42d09eec700df94d994e
OscarBorges <oscar.borgesfs@gmail.com>
2020-08-26 18:58:06 -0300

    Adicionada informação sobre contribuição a projetos de codigos abertos

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ standard_exclude_directories = [
 ]
 
 
+
 # (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 # Note: you may want to copy this into your setup.py file verbatim, as

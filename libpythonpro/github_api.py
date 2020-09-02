efa125db4cc6f642ada5e6dae3c1997409fcd5a3
avellar75 <evandro.avellar@gmail.com>
2020-09-01 22:04:13 -0300

    atualizando o .flake8

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -9,5 +9,12 @@ def busca_avatar(usuario):
     return resp.json()['avatar_url']
 
 
+def busca_nome(usuario):
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['name']
+
+
 if __name__ == '__main__':
     print(busca_avatar('avellar1975'))
+    print(busca_nome('avellar1975'))

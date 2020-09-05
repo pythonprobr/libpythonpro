6f2fcbe8d171cedbb66736d2f3d821cfe77c33af
avellar1975 <evandro.avellar@gmail.com>
2020-09-05 13:43:22 -0300

    Testes para cobertura

--- libpythonpro/tests/test_exemplo.py
+++ libpythonpro/tests/test_exemplo.py
@@ -1,2 +1,11 @@
-def test_int():
-    assert 1 == 1
+import pytest
+from libpythonpro.github_api import busca_avatar
+
+
+@pytest.mark.parametrize(
+    'usuario',
+    ['avellar1975', 'renzo']
+)
+def test_busca_avatar(usuario):
+    resultado = busca_avatar(usuario)
+    assert resultado is not None

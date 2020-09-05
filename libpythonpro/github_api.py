fdcc29d76beffa1a0294597172a38f9bad45068a
avellar1975 <evandro.avellar@gmail.com>
2020-09-05 13:53:56 -0300

    Retirando testes do codigo principal

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -13,8 +13,3 @@ def busca_nome(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['name']
-
-
-if __name__ == '__main__':
-    print(busca_avatar('avellar1975'))
-    print(busca_nome('avellar1975'))

d751ac121f4f9c1062d4a6feb5dc1c795fa83950
avellar1975 <evandro.avellar@gmail.com>
2020-09-05 13:50:17 -0300

    Aumentando a cobertura de teste

--- libpythonpro/tests/test_exemplo.py
+++ libpythonpro/tests/test_exemplo.py
@@ -1,5 +1,5 @@
 import pytest
-from libpythonpro.github_api import busca_avatar
+from libpythonpro.github_api import busca_avatar, busca_nome
 
 
 @pytest.mark.parametrize(
@@ -8,4 +8,9 @@ from libpythonpro.github_api import busca_avatar
 )
 def test_busca_avatar(usuario):
     resultado = busca_avatar(usuario)
+    assert resultado is not None
+
+
+def test_busca_nome():
+    resultado = busca_nome('avellar1975')
     assert resultado is not None

266a27a416b7325ee2aeb871e32e382228435faa
avellar1975 <evandro.avellar@gmail.com>
2020-09-06 16:46:53 -0300

    corrigindo os testes do github_api.py

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -3,13 +3,13 @@
 import requests
 
 
-def busca_avatar(usuario):
+def buscar_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
 
 
-def busca_nome(usuario):
+def buscar_nome(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['name']
--- libpythonpro/tests/test_exemplo.py
+++ libpythonpro/tests/test_exemplo.py
@@ -1,16 +1,16 @@
 import pytest
-from libpythonpro.github_api import busca_avatar, busca_nome
+from libpythonpro.github_api import buscar_avatar, buscar_nome
 
 
 @pytest.mark.parametrize(
     'usuario',
     ['avellar1975', 'renzo']
 )
-def test_busca_avatar(usuario):
-    resultado = busca_avatar(usuario)
+def test_buscar_avatar(usuario):
+    resultado = buscar_avatar(usuario)
     assert resultado is not None
 
 
-def test_busca_nome():
-    resultado = busca_nome('avellar1975')
+def test_buscar_nome():
+    resultado = buscar_nome('avellar1975')
     assert resultado is not None

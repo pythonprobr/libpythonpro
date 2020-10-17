23454b16407a959a26c6f33c68b4de6080f97d00
Carolina Oliveira <carolinaoliveira@id.uff.br>
2020-10-17 19:05:26 -0300

    Módulo de aprendizagem do PyTools. Importando requests.

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -11,3 +11,6 @@ def buscar_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print (buscar_avatar('carololiveira6'))

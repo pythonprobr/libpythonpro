518ac7ba559d6b7d1e77e4008e55c8c050864a96
JameicaAlvarenga <jnbrag@gmail.com>
2020-12-04 15:52:45 -0300

    api que retorna o avatar dado um usuário do github

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,17 @@
+import requests
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar do github
+    
+    :param usuario: str com nome de usuario no github
+    :return: str com link do avatar
+    """
+
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('renzon'))

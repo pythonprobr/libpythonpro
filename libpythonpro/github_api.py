4424949f12d2bb58ce24f415efc686e3ea1f4e40
SuperFeliz <linuxeopoder@gmail.com>
2020-12-22 11:33:58 -0300

    Removido espaço em branco do comentário

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -4,7 +4,6 @@ import requests
 def buscar_avatar(usuario):
     """
     Busca o avatar de um usuário no Github
-
     :param usuario: str com o nome de usuário no github
     :return: str com o link do avatar
     """

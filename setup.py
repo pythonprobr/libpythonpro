e7d366f964338cdc68bfe41d355e568ee148b426
JameicaAlvarenga <jnbrag@gmail.com>
2020-12-24 10:52:44 -0300

    Alerando nome do pacote

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python 3.6",
+        "Programming Language :: Python 3.0",
         "Framework :: Pytest",
     ],
     install_requires=[

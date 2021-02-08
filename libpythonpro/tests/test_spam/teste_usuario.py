9ce4a4519336e1aac01caa9cde8312b71b3b838a
JameicaAlvarenga <jnbrag@gmail.com>
2021-02-08 18:09:21 -0300

    gestão de dependencias

--- libpythonpro/tests/test_spam/teste_usuario.py
+++ libpythonpro/tests/test_spam/teste_usuario.py
@@ -1,1 +1,42 @@
+class Sessao:
+    contador = 0
+    usuarios=[]
+
+    def salvar(self, usuario):
+        Sessao.contador +=1
+        usuario.id = Sessao.contador
+        self.usuarios.append(usuario)
+
+    def listar(self):
+        return self.listar()
+
+class Conexao():
+    def gerar_sessao(self)
+        return Sessao()
+
+
+class Usuario:
+    def __init__(self,nome):
+        pass
+
+
 def test_salvar_usuario():
+   conexao = Conexao()
+   sessao = conexao.gerar_sessao()
+   Usuario=Usuario(nome='Renzo')
+   sessao.salvar(usuario)
+   assert isinstance(usuario.id, int)
+   sessao.roll_back()
+   sessao.fechar()
+   conexao.fechar()
+
+def test_listar_usuarios():
+   conexao = Conexao()
+   sessao = conexao.gerar_sessao()
+   Usuario=[Usuario(nome='Renzo'),Usuario(nome='Luciano')]
+   for usuario in usuarios:
+       sessao.salvar(usuario)
+   assert usuario== sessao.listar()
+   sessao.roll_back()
+   sessao.fechar()
+   conexao.fechar()

6da95547db40f56ea07d0b11958ebe7f213111cb
JameicaAlvarenga <jnbrag@gmail.com>
2021-02-25 00:07:30 -0300

    teste com banco de dados

--- libpythonpro/spam/main.py
+++ libpythonpro/spam/main.py
@@ -1,2 +1,8 @@
 class EnviadorDeSpam(object):
-    pass
+    def __init__(self, sessao, enviador):
+        self.sessao = sessao
+        self.enviador = enviador
+
+    def enviar_emails(self, remetente, assunto, corpo):
+        pass
+

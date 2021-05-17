2b54dac09061ab4c2e5b68584317ab5dcc6c773c
leandro de menezes martins <leandro.m.martins.info@gmail.com>
2021-05-17 09:49:24 -0300

    aula de criação da chamada de api

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -7,7 +7,11 @@ def buscar_avatar(usuario):
 
     :param usuario: str com o nome de usuário no github
     :return: str com o link do avatar
+    https://api.github.com/users/leandromartins36
     """
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('leandromartins36'))

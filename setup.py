1a00e22bb466f0cb53b5f8af15f5bc14291a6b0c
Geilson25 <geilsonfidelis@egmail.com>
2021-05-27 10:25:19 -0300

    Inserido Test com Pytest.

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Renzo Nuccitelli"
 AUTHOR_EMAIL = "renzo@python.pro.br"
-URL = "https://github.com/pythonprobr/libpythonpro"
+URL = "https://github.com/geilson25/libpythonpro"
 VERSION = __import__(PACKAGE).__version__
 
 setup(
@@ -117,7 +117,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license="GNU AFFERO GENERAL PUBLIC LICENSE",
+    license=read(LICENSE),
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),

e47dc2d87a373ddaae8af9208aad7fa84f66a55d
charlesnoamlael <nasa0147>
2021-06-23 17:00:04 -0300

    configurado o Travis.ci suportado python 3

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,34 +1,20 @@
-# import requests
-#
-#
-# def buscar_avatar(usuario):
-#     """
-#     Busca o avatar de um usuário no Github
-#
-#     :param usuario: str com o nome de usuário no github
-#     :return: str com o link do avatar
-#     """
-#     url = f'https://api.github.com/users/{usuario}'
-#     resp = requests.get(url)
-#     return resp.json()['avatar_url']
-#
-#
-# print(buscar_avatar('charlesnoamlael'))
-
 import requests
 
 
 def buscar_avatar(usuario):
     """
-    Busca o avatar de um usuario no Github
+    Busca o avatar de um usuário no Github
 
-    :param: usuario: str com o nome de usuario no git hub
-    return: str com o link do avatar
+    :param usuario: str com o nome de usuário no github
+    :return: str com o link do avatar
     """
-
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
 
 
 print(buscar_avatar('charlesnoamlael'))
+
+
+
+

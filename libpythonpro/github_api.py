f1ae3b95bfc9d15ded0a2ca845275ca91a800b9d
4n6ics <marsamb@gmail.com>
2021-07-27 15:13:06 -0300

    Changed JDK

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -10,6 +10,7 @@ def buscar_avatar(usuario):
     """
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
+    print(resp.status_code)
     return resp.json()['avatar_url']
 
 

073dce27697d1f3f16638145e7aaab816d1de5bd
Leal <adrianolealnascimento@gmail.com>
2021-11-21 15:23:49 -0300

    Adicionando dependencia

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,13 +1,17 @@
 import requests
 
 
-def buscar_avatar(usuario):
+
+def buscar_avatar(Adrand):
     """
     Busca o avatar de um usuário no Github
-
     :param usuario: str com o nome de usuário no github
     :return: str com o link do avatar
     """
-    url = f'https://api.github.com/users/{usuario}'
+    url = f'https://api.github.com/users/{Adrand}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('Adrand'))

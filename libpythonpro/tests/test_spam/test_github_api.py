89459c75761333aab8f196e5fb155ee5d3d1a301
Rawston Marinho Pinto <62628200+Rawston@users.noreply.github.com>
2021-12-26 13:06:54 -0300

    Fim do test

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,12 +1,29 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
-def test_buscar_avatar():
+
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/62628200?v=4'
     resp_mock.json.return_value = {
-        'login':'Rawston','id': 62628200,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/62628200?v=4',
+        'login': 'Rawston', 'id': 62628200,
+        'avatar_url': url,
     }
-    github_api.requests.get = Mock(return_value=resp_mock)
+    # Salvamento do get original
+    get_mock=mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value=resp_mock
+    return url
+
+
+# Sem acesso a rede
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('Rawston')
+    assert 'https://avatars.githubusercontent.com/u/62628200?v=4' == url
+
+# Com acesso a rede
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('Rawston')
     assert 'https://avatars.githubusercontent.com/u/62628200?v=4' == url

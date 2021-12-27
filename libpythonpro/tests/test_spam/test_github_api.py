f65b8c042ae276b68629781821eab0bbaa4731d0
Rawston Marinho Pinto <62628200+Rawston@users.noreply.github.com>
2021-12-27 01:34:12 -0300

    Alterado gestor de dependências para pipenv
    close #626

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,20 +1,21 @@
-from unittest.mock import Mock
+import unittest.mock
 
 import pytest
 
 from libpythonpro import github_api
 
+
 @pytest.fixture
 def avatar_url(mocker):
-    resp_mock = Mock()
+    resp_mock = unittest.mock.Mock()
     url = 'https://avatars.githubusercontent.com/u/62628200?v=4'
     resp_mock.json.return_value = {
         'login': 'Rawston', 'id': 62628200,
         'avatar_url': url,
     }
     # Salvamento do get original
-    get_mock=mocker.patch('libpythonpro.github_api.requests.get')
-    get_mock.return_value=resp_mock
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
     return url
 
 
@@ -22,6 +23,7 @@ def avatar_url(mocker):
 def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('Rawston')
     assert 'https://avatars.githubusercontent.com/u/62628200?v=4' == url
+
 
 # Com acesso a rede
 def test_buscar_avatar_integracao():

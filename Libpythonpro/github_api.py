b424cd73b2434a2c68af6566a0b00f51e52fccb2
Carloshbfreire <carhb@outlook.com>
2022-08-22 16:21:35 -0300

    Adicionada flake8 como dependência
    close #12

---
+++ Libpythonpro/github_api.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário no Github
+    :Param usuário: str com o nome usuário no github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('Carloshbfreire'))
--- README.md
+++ README.md
@@ -16,14 +16,15 @@ Suportada versão 3 de Python Carlinhos
 Para instalar:
 
 ```console
-pip install pipenv
-pipenv install --dev
+python3 -m venv .venv
+source .venv/bin/activete
+pip install -r requirements-dev.txt
 ```
 
 Para conferir qualidade de código:
 
 ```console
-pipenv run flake8
+flake8
 ```
 
 Tópicos a serem abordados:

992fc186ec27a7898e81d8c43782c5b4b79dd61d
Carloshbfreire <carhb@outlook.com>
2022-09-28 15:07:16 -0300

    Terminado o Exercicio de Injeção de dependência.

--- Libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ Libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -4,22 +4,37 @@ from Libpythonpro.spam.main import EnviadorDeSpam
 from Libpythonpro.spam.modelos import Usuario
 from Libpythonpro.spam.test_spam.enviador_de_email import Enviador
 
+
+def test_envio_de_spam(sessao):
+    enviador_de_spam= EnviadorDeSpam(sessao, Enviador())
+
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatário, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatário, assunto, corpo)
+        self.qtd_email_enviados += 1
+
 @pytest.mark.parametrize(
     'usuarios',
     [
         [
             Usuario(nome='Carlos', email='carhb@outlook.com'),
-            Usuario(nome='Givani', email='carhb@outlook.com')
+            Usuario(nome='Givani', email='givanifcf@hotmail.com')
         ],
         [
             Usuario(nome='Carlos', email='carhb@outlook.com')
         ]
     ]
 )
-def test_qde_de_spam(sessao, usuarios):
+def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam= EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'carhb@outlook.com',
@@ -27,5 +42,24 @@ def test_qde_de_spam(sessao, usuarios):
         'Confira os módulos fantásticos'
     )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Carlos', email='carhb@outlook.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam= EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'givanifcf@hotmail.com',
+        'Curso Pythonpro',
+        'Confira os módulos fantásticos'
+    )
+    assert enviador.parametros_de_envio ==(
+        'givanifcf@hotmail.com',
+        'carhb@outlook.com',
+        'Curso Pythonpro',
+        'Confira os módulos fantásticos'
+    )
+
 
 

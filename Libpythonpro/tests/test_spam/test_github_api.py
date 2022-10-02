8c367bf1c08da119907ad17c3eca20fdb51b4cd7
Carloshbfreire <carhb@outlook.com>
2022-10-02 16:35:42 -0300

    Terminado o Exercicio de Isolamento de Imports

--- Libpythonpro/tests/test_spam/test_github_api.py
+++ Libpythonpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,26 @@
 from unittest.mock import Mock
 
+import pytest
+
 from Libpythonpro import github_api
 
-
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/107650943?v=4'
     resp_mock.json.return_value = {
         'login': 'Carloshbfreire', 'id': 107650943,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/107650943?v=4',
+        'avatar_url': url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('Carloshbfreire')
+    assert avatar_url == url
+
+def test_buscar_avatar_integraçao():
     url = github_api.buscar_avatar('Carloshbfreire')
     assert 'https://avatars.githubusercontent.com/u/107650943?v=4' == url
